2dbe645f351ceb28d4a39c72254eb72007fc98aa
Bojan Božić <boj.bozic@gmail.com>
2018-04-16 19:15:23 +0100

    new

---
+++ timeplots.R
@@ -0,0 +1,9 @@
+require(astsa)
+plot(jj,type='o',main='Johnson&Johnson quarterly earnings per share',
+     ylab='Earnings',xlab='Years')
+
+plot(flu,main='Monthly Pneumonia and Influenza Deaths in US',
+     ylab='Number of Deaths per 10,000 people',xlab='Months')
+
+plot(globtemp,main='Global mean land-ocean dev from avg temp 1951-1980',
+     ylab='Temperature deviations',type='o',xlab='Years')
