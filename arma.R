d16a410c8fbc901dc63fe0824e0fcdd268c08fa6
Bojan Božić <boj.bozic@gmail.com>
2019-01-15 10:35:54 +0000

    new

---
+++ arma.R
@@ -0,0 +1,12 @@
+set.seed(90210)
+x = arima.sim(list(order = c(1,0,1), ar =.9, ma=.5), n = 100)
+xr = rev(x) # xr is the reversed data
+pxr = predict(arima(xr, order=c(1,0,1)), 10) # predict the reversed data
+pxrp = rev(pxr$pred) # reorder the predictors (for plotting)
+pxrse = rev(pxr$se) # reorder the SEs
+nx = ts(c(pxrp, x), start=-9) # attach the backcasts to the data
+plot(nx, ylab=expression(X[~t]), main='Backcasting')
+U = nx[1:10] + pxrse; L = nx[1:10] - pxrse
+xx = c(-9:0, 0:-9); yy = c(L, rev(U))
+polygon(xx, yy, border = 8, col = gray(0.6, alpha = 0.2))
+lines(-9:0, nx[1:10], col=2, type='o')
